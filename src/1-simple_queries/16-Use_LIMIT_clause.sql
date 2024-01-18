bd879795762baccf521ef3e35acccad72517f980
Ali Daghighi <daghighi1376@gmail.com>
2024-01-18 17:35:45 +0300

    Update 16-Use_LIMIT_clause.sql

--- src/1-simple_queries/16-Use_LIMIT_clause.sql
+++ src/1-simple_queries/16-Use_LIMIT_clause.sql
@@ -4,12 +4,13 @@ FROM User
 ORDER BY registration_date
 LIMIT 5;
 
--- Query 2: Select courses with at most 3 modules
+-- Query 2: Select any 5 courses with at most 3 modules
 SELECT c.course_id, c.course_name, COUNT(cm.module_id) AS total_modules
 FROM Course c
 LEFT JOIN CourseModule cm ON c.course_id = cm.course_id
 GROUP BY c.course_id, c.course_name
-HAVING total_modules <= 3;
+HAVING total_modules <= 3
+LIMIT 5;
 
 -- Query 3: Select the latest 10 submissions
 SELECT *
