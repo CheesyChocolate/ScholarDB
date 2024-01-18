0bb79a3ad963d80bd3f9edc04ba957a013c10a6c
CheesyChocolate <dev@behnamlal.xyz>
2024-01-18 14:55:02 +0000

    add(simple_queries): add `query_all_tables.sql`

---
+++ src/1-simple_queries/00-query_all_tables.sql
@@ -0,0 +1,12 @@
+-- Query to retrieve data from all tables
+SELECT * FROM User;
+SELECT * FROM Role;
+SELECT * FROM UserRole;
+SELECT * FROM UserProfile;
+SELECT * FROM Course;
+SELECT * FROM CourseModule;
+SELECT * FROM ModuleContent;
+SELECT * FROM Enrollment;
+SELECT * FROM Grade;
+SELECT * FROM Submission;
+SELECT * FROM UserCourseEnrollment;
